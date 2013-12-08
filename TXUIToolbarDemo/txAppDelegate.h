cb90b7db733c6ebdae1b8fcb29b3c2cae6881f17
萱的软件测试博客 <qiuhmm@gmail.com>
2013-12-08 20:38:53 +0800

    Initial Commit

---
+++ TXUIToolbarDemo/txAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  txAppDelegate.h
+//  TXUIToolbarDemo
+//
+//  Created by tingxuan.qhm on 13-12-8.
+//  Copyright (c) 2013年 tingxuan.qhm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface txAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
